b081d3db58cf6dfdca9c952ce43b0761325010ce
jvmm2005 <145668170+jvmm2005@users.noreply.github.com>
2023-10-05 21:54:25 +0200

    Add files via upload

---
+++ Kinetic-Energy.py
@@ -0,0 +1,4 @@
+mass = float(input("Enter the mass (in kilograms): ")) #float = números
+velocity = float(input("Enter the velocity (in m/s): "))
+KE = (0.5*mass*velocity*velocity)
+print (KE)
---
+++ temperature.py
@@ -0,0 +1,5 @@
+#Temperaturas
+temperature_celsius = 99.5
+print("la temperatura en C", temperature_celsius)
+temperature_fahrenheit = (temperature_celsius*9/5)+32
+print ("la temperatura en F", temperature_fahrenheit)
